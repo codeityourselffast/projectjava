a200265fde7d39d8a72c57b95d8cc98c39a47c14
codeityourselffast <ismaelboushaba@gmail.com>
2022-01-13 09:21:46 +0100

    tp1 13 01

---
+++ Project/src/Aeroport.java
@@ -0,0 +1,38 @@
+public class Aeroport
+{
+
+    String IATA;
+    String Name;
+    String country;
+    String latitude;
+    String longitude;
+
+    Aeroport()
+    {
+
+    }
+
+    public void public void getIATA()
+    {
+
+    }
+
+    public void getLatitude()
+    {
+
+    }
+
+    public void getLongitude()
+    {
+
+    }
+/*
+    public void toString()
+    {
+
+    }
+
+ */
+ 
+}
+
---
+++ Project/src/World.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class World
+{
+    ArrayList<String>Aeroport;
+
+    public void Wordl()
+    {
+
+    }
+
+    public void findByCode()
+    {
+
+    }
+
+    public void findNearestAirport()
+    {
+
+    }
+
+    public void getList()
+    {
+
+    }
+    public void main()
+    {
+
+    }
+
+    public void distance()
+    {
+
+    }
+}
